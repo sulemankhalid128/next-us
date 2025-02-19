867c8c78785d97c895ab8c13256cc797b2c19a57
sulemankhalid128 <sulemankhalid128@gmail.com>
2025-02-19 17:56:23 +0500

    fixes

--- src/app/(auth)/forget-password/page.tsx
+++ src/app/(auth)/forget-password/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>Forgot page</div>
-  )
-}
+    <>
+      <div>Forgot page</div>
+      <Link href="/login">Back to login </Link>
+    </>
+  );
+};
 
-export default page
+export default page;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <>
+      <div>Login Page</div>
+      <Link href='/forget-password'>Forget Password </Link>
+    </>
+  );
+};
 
-export default page
+export default page;
